8f1bf35af58f018923e8e3f3664a1bcbaf72b4f9
codemiguel-dev <jose.calderon15@inacapmail.cl>
2024-12-28 03:37:17 -0300

    test

--- MyForm.h
+++ MyForm.h
@@ -24,8 +24,29 @@ namespace CLRStuff {
     private:
         void InitializeComponent(void)
         {
-            // Código de inicialización de componentes aquí
+            this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+            this->SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this->checkBox1->AutoSize = true;
+            this->checkBox1->Location = System::Drawing::Point(424, 73);
+            this->checkBox1->Name = L"checkBox1";
+            this->checkBox1->Size = System::Drawing::Size(80, 17);
+            this->checkBox1->TabIndex = 0;
+            this->checkBox1->Text = L"checkBox1";
+            this->checkBox1->UseVisualStyleBackColor = true;
+            // 
+            // MyForm
+            // 
+            this->ClientSize = System::Drawing::Size(824, 261);
+            this->Controls->Add(this->checkBox1);
+            this->Name = L"MyForm";
+            this->ResumeLayout(false);
+            this->PerformLayout();
+
         }
+    private: System::Windows::Forms::CheckBox^ checkBox1;
 
     private:
         System::ComponentModel::Container^ components;
---
+++ MyForm1.h
@@ -0,0 +1,80 @@
+#pragma once
+
+namespace CLRdesktopnet {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm1
+	/// </summary>
+	public ref class MyForm1 : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::CheckBox^ checkBox1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->SuspendLayout();
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Location = System::Drawing::Point(111, 80);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(80, 17);
+			this->checkBox1->TabIndex = 0;
+			this->checkBox1->Text = L"checkBox1";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			// 
+			// MyForm1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->checkBox1);
+			this->Name = L"MyForm1";
+			this->Text = L"MyForm1";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	};
+}
