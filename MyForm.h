b32525bd17254cbb0990c698467debcd67562dd4
codemiguel-dev <jose.calderon15@inacapmail.cl>
2024-12-28 03:58:21 -0300

    test

--- MyForm.h
+++ MyForm.h
@@ -24,22 +24,22 @@ namespace CLRStuff {
     private:
         void InitializeComponent(void)
         {
-            this->button1 = (gcnew System::Windows::Forms::Button());
+            this->btn_message = (gcnew System::Windows::Forms::Button());
             this->label1 = (gcnew System::Windows::Forms::Label());
             this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+            this->btn_status = (gcnew System::Windows::Forms::Button());
+            this->label2 = (gcnew System::Windows::Forms::Label());
             this->SuspendLayout();
-
             // 
-            // button1
+            // btn_message
             // 
-            this->button1->Location = System::Drawing::Point(106, 119);
-            this->button1->Name = L"button1";
-            this->button1->Size = System::Drawing::Size(127, 23);
-            this->button1->TabIndex = 1;
-            this->button1->Text = L"Mostrar Mensaje";
-            this->button1->UseVisualStyleBackColor = true;
-            this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
-
+            this->btn_message->Location = System::Drawing::Point(106, 119);
+            this->btn_message->Name = L"btn_message";
+            this->btn_message->Size = System::Drawing::Size(127, 23);
+            this->btn_message->TabIndex = 1;
+            this->btn_message->Text = L"Mostrar Mensaje";
+            this->btn_message->UseVisualStyleBackColor = true;
+            this->btn_message->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
             // 
             // label1
             // 
@@ -48,36 +48,60 @@ namespace CLRStuff {
                 static_cast<System::Byte>(0)));
             this->label1->Location = System::Drawing::Point(99, 22);
             this->label1->Name = L"label1";
-            this->label1->Size = System::Drawing::Size(586, 39);
+            this->label1->Size = System::Drawing::Size(594, 39);
             this->label1->TabIndex = 2;
             this->label1->Text = L"Bienvenido curso de C++ de escritorio";
-
             // 
             // checkBox1
             // 
             this->checkBox1->AutoSize = true;
             this->checkBox1->Location = System::Drawing::Point(286, 124);
             this->checkBox1->Name = L"checkBox1";
-            this->checkBox1->Size = System::Drawing::Size(80, 17);
+            this->checkBox1->Size = System::Drawing::Size(104, 17);
             this->checkBox1->TabIndex = 3;
             this->checkBox1->Text = L"Mostrar Mensaje";
             this->checkBox1->UseVisualStyleBackColor = true;
-
+            // 
+            // btn_status
+            // 
+            this->btn_status->Location = System::Drawing::Point(106, 166);
+            this->btn_status->Name = L"btn_status";
+            this->btn_status->Size = System::Drawing::Size(127, 23);
+            this->btn_status->TabIndex = 4;
+            this->btn_status->Text = L"Cambiar estado";
+            this->btn_status->UseVisualStyleBackColor = true;
+            this->btn_status->Click += gcnew System::EventHandler(this, &MyForm::button1_Click_1);
+            // 
+            // label2
+            // 
+            this->label2->AutoSize = true;
+            this->label2->Location = System::Drawing::Point(286, 166);
+            this->label2->Name = L"label2";
+            this->label2->Size = System::Drawing::Size(63, 13);
+            this->label2->TabIndex = 5;
+            this->label2->Text = L"label_status";
             // 
             // MyForm
             // 
             this->ClientSize = System::Drawing::Size(824, 261);
+            this->Controls->Add(this->label2);
+            this->Controls->Add(this->btn_status);
             this->Controls->Add(this->checkBox1);
             this->Controls->Add(this->label1);
-            this->Controls->Add(this->button1);
+            this->Controls->Add(this->btn_message);
             this->Name = L"MyForm";
             this->ResumeLayout(false);
             this->PerformLayout();
-        }
 
-    private: System::Windows::Forms::Button^ button1;
+        }
+    private: System::Windows::Forms::Button^ btn_message;
+
+
     private: System::Windows::Forms::Label^ label1;
     private: System::Windows::Forms::CheckBox^ checkBox1;
+    private: System::Windows::Forms::Button^ btn_status;
+
+    private: System::Windows::Forms::Label^ label2;
 
     private:
         System::ComponentModel::Container^ components;
@@ -96,5 +120,9 @@ namespace CLRStuff {
             MessageBox::Show("El CheckBox está desmarcado.", "Estado del CheckBox", MessageBoxButtons::OK, MessageBoxIcon::Information);
         }
     }
-    };
+
+    private: System::Void button1_Click_1(System::Object^ sender, System::EventArgs^ e) {
+        label2->Text = "hello";
+    }
+};
 }
