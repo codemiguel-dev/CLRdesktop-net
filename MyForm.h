91f9914d65304fb74d6fbf3a1751b0aed3ed533c
codemiguel-dev <jose.calderon15@inacapmail.cl>
2024-12-28 03:38:12 -0300

    test

--- MyForm.h
+++ MyForm.h
@@ -25,6 +25,7 @@ namespace CLRStuff {
         void InitializeComponent(void)
         {
             this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+            this->button1 = (gcnew System::Windows::Forms::Button());
             this->SuspendLayout();
             // 
             // checkBox1
@@ -37,9 +38,19 @@ namespace CLRStuff {
             this->checkBox1->Text = L"checkBox1";
             this->checkBox1->UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this->button1->Location = System::Drawing::Point(437, 141);
+            this->button1->Name = L"button1";
+            this->button1->Size = System::Drawing::Size(127, 23);
+            this->button1->TabIndex = 1;
+            this->button1->Text = L"button1";
+            this->button1->UseVisualStyleBackColor = true;
+            // 
             // MyForm
             // 
             this->ClientSize = System::Drawing::Size(824, 261);
+            this->Controls->Add(this->button1);
             this->Controls->Add(this->checkBox1);
             this->Name = L"MyForm";
             this->ResumeLayout(false);
@@ -47,6 +58,7 @@ namespace CLRStuff {
 
         }
     private: System::Windows::Forms::CheckBox^ checkBox1;
+    private: System::Windows::Forms::Button^ button1;
 
     private:
         System::ComponentModel::Container^ components;
